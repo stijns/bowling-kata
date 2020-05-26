b7b4c74d8eda5d45c4fa0a1a931d270c38023e8c
stijn <stijn.slaets@gmail.com>
2020-05-26 14:33:10 +0200

    A simple implementation of the bowling kata

--- src/main/java/kata/tdd/bowling/Game.java
+++ src/main/java/kata/tdd/bowling/Game.java
@@ -3,4 +3,38 @@ package kata.tdd.bowling;
 public class Game {
 
 
+    private final int[] rolls = new int[20];
+    private int rollIndex = 0;
+
+    public int getScore() {
+        int score = 0;
+        int rollIndex = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            if (isStrike(rollIndex)) {
+                score = score + 10 + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                rollIndex += 1;
+            } else if (isSpare(rollIndex)) {
+                score = score + rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+                rollIndex += 2;
+            } else {
+                score = score + rolls[rollIndex] + rolls[rollIndex + 1];
+                rollIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    private boolean isStrike(int rollIndex) {
+        return rolls[rollIndex] == 10;
+    }
+
+    private boolean isSpare(int rollIndex) {
+        return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+    }
+
+
+    public void roll(int pins) {
+        rolls[rollIndex++] = pins;
+    }
+
 }
--- src/test/java/kata/tdd/bowling/GameTest.java
+++ src/test/java/kata/tdd/bowling/GameTest.java
@@ -1,6 +1,58 @@
 package kata.tdd.bowling;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GameTest {
 
+    private Game g;
+
+    @BeforeEach
+    void setupGame() {
+        g = new Game();
+    }
+
+    @Test
+    void testNullGame() {
+        rollMany(20, 0);
+        Assertions.assertEquals(0, g.getScore());
+    }
+
+    @Test
+    void testAllOnes() {
+        rollMany(20, 1);
+        Assertions.assertEquals(20, g.getScore());
+    }
+
+    @Test
+    void testOneSpare() {
+        g.roll(1);
+        g.roll(9); //spare
+        g.roll(3); //counts double
+        rollMany(17, 0);
+        Assertions.assertEquals(16, g.getScore());
+    }
+
+    @Test
+    void testOneStrike() {
+        g.roll(10); //strike
+        g.roll(2); //1st roll 2nd frame counts double
+        g.roll(3); //2nd roll 2nd frame counts double
+        rollMany(17, 0);
+        Assertions.assertEquals(20, g.getScore());
+    }
+
+    @Test
+    void testPerfectGame(){
+        rollMany(12,10);
+        Assertions.assertEquals(300, g.getScore());
+    }
+
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            g.roll(pins);
+        }
+    }
 
 }
